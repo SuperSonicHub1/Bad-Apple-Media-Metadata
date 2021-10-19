8451949834aa7be41e0239b07c9ce919f2098549
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2021-10-19 17:32:31 +0000

    Added types and experimental "timeupdate" mode.

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ if ('mediaSession' in navigator) {
   })
 }
 
+/** @type {HTMLVideoElement} */
 const video = document.getElementById("video")
+/** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext('2d')
+
+// Takes up the UI thread less, but it's more choppy
+// video.addEventListener("timeupdate", () => {
+// 	ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+// 	navigator.mediaSession.metadata.artwork = [
+// 		{ src: canvas.toDataURL(), type: "image/png" }
+// 	]
+// })
 
 function update() {
   if (video.paused || video.ended) return
